2d76093a01d56564f40f90bf813bc6307dc6fcba
Jonathan Hsu <jon@high5.ai>
2019-03-16 21:42:10 +0800

    add post

--- app.js
+++ app.js
@@ -3,15 +3,21 @@ const https = require('https')
 const express = require('express')
 
 const app = express()
+const tars_host = 'http://10.138.0.3:3000/'
+
+app.get('/', (res, req) => {
+    req.send('valid')
+})
+
+app.post('/', async (res, req) => {
+    await axios.post(tars_host + 'webhook')
+    res.sendStatus(200)
+})
 
 const options = {
     key: fs.readFileSync('.ssl/private.key'),
     cert: fs.readFileSync('.ssl/certificate.crt')
 }
-
-app.get('/', (res, req) => {
-    req.send('secure')
-})
 
 https.createServer(options, app).listen(443, () => {
     console.log('tars-webhook listening on port 3000!')
